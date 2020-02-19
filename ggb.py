f0f9dc15f44f9d4f319a8f02748e08c77d8a13c7
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-02-19 20:48:33 +0700

    add comments

--- ggb.py
+++ ggb.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #read original
-ori_img = cv2.imread('apple.jpg')
+ori_img = cv2.imread('paper.png')
 
 #OpenCV display image using BGR space by default.
 #Therefore. we should it to RGB to achieve orignal image.
@@ -17,7 +17,7 @@ r = ori_img[:, :, 0].copy()
 g = ori_img[:, :, 1].copy()
 b = ori_img[:, :, 2].copy()
 
-#contrast enhance g channel with constant of 1.5
+#contrast enhance g channel with constant of 1.2
 for i in range(g.shape[0]) :
     for j in range(g.shape[1]) :
         if g[i][j] * 1.2 > 255 :
@@ -27,9 +27,9 @@ for i in range(g.shape[0]) :
 
 #output image
 output_img = np.ndarray(shape=ori_img.shape, dtype=int)
-output_img[:, :, 0] = g
-output_img[:, :, 1] = g
-output_img[:, :, 2] = b
+output_img[:, :, 0] = b
+output_img[:, :, 1] = b
+output_img[:, :, 2] = g
 
 #display original image and its output
 images = [ori_img, output_img]
